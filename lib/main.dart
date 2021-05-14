62097a091b77445837bf91cc2ba1855649d98432
Safiullah Parvez <safiullah.parvez123@gmail.com>
2021-05-14 18:13:51 +0500

    flutter project

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Text("Safiullah Parvez Memon safiullah.parvez123@gmail.com"),
+      ),
+      
+    );
+  }
+}
